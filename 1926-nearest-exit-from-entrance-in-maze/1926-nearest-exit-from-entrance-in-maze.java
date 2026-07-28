c00103ab94d6f1bd66e1f84f8cd6a08cc51f8fe7
BIKRAM GHOSH <ghoshbikram2725@gmail.com>
2026-07-28 18:05:24 +0530

    Time: 7 ms (61.84%), Space: 48.5 MB (12.83%) - LeetHub

---
+++ 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.java
@@ -0,0 +1,63 @@
+class Solution {
+    public int nearestExit(char[][] maze, int[] entrance) {
+        
+        int rows = maze.length;
+        int cols = maze[0].length;
+
+        Queue<int[]> q = new LinkedList<>();
+
+        q.offer(new int[]{entrance[0],entrance[1]});
+
+        maze[entrance[0]][entrance[1]] = '+';
+
+        int[][] dir =
+        {
+            {-1,0},
+            {1,0},
+            {0,-1},
+            {0,1}
+        };
+
+        int steps = 0;
+
+        while(!q.isEmpty())
+        {
+            int Qsize = q.size();
+
+            for(int i = 0 ; i < Qsize ;i++)
+            {
+                int[] curr = q.poll();
+
+                int r = curr[0];
+                int c = curr[1];
+
+                for(int[] d : dir)
+                {
+                    int nr = r + d[0];
+                    int nc = c + d[1];
+
+                    if (nr < 0 || nr >= rows || nc < 0 || nc >= cols)
+                    {
+                        continue;
+                    }
+
+                    if (maze[nr][nc] == '+')
+                    {
+                        continue;
+                    }
+
+                    if (nr == 0 || nr == rows - 1 || nc == 0 || nc == cols - 1)
+                    {
+                        return steps + 1;
+                    }
+                    maze[nr][nc] = '+';
+
+                    q.offer(new int[]{nr, nc});
+                }
+            }
+            steps++;
+        }
+        return -1;
+
+    }
+}
