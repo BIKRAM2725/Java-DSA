80dd9e03840892ca4a995dd6b71266154ad370f1
BIKRAM GHOSH <ghoshbikram2725@gmail.com>
2026-07-29 18:10:27 +0530

    Time: 7 ms (61.7%), Space: 48.3 MB (38.82%) - LeetHub

--- 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.java
+++ 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.java
@@ -1,63 +1,67 @@
 class Solution {
     public int nearestExit(char[][] maze, int[] entrance) {
         
-        int rows = maze.length;
-        int cols = maze[0].length;
+    int rows = maze.length;
+    int cols = maze[0].length;
 
-        Queue<int[]> q = new LinkedList<>();
+    Queue<int[]> q = new LinkedList<>();
 
-        q.offer(new int[]{entrance[0],entrance[1]});
+    q.offer(new int[]{entrance[0],entrance[1]});
 
-        maze[entrance[0]][entrance[1]] = '+';
+    maze[entrance[0]][entrance[1]] = '+';
 
-        int[][] dir =
+    int[][] dir = {
+        {1,0},
+        {0,1},
+        {-1,0},
+        {0,-1}
+    };
+
+    int steps = 0;
+
+    while(!q.isEmpty())
+    {
+        int Qsize = q.size();
+
+        for(int i = 0 ; i < Qsize ; i++)
         {
-            {-1,0},
-            {1,0},
-            {0,-1},
-            {0,1}
-        };
+            int[] curr = q.poll();
 
-        int steps = 0;
+            int r = curr[0];
+            int c = curr[1];
 
-        while(!q.isEmpty())
-        {
-            int Qsize = q.size();
-
-            for(int i = 0 ; i < Qsize ;i++)
+            for(int[] d : dir)
             {
-                int[] curr = q.poll();
+                int nr = r + d[0];
+                int nc = c + d[1];
 
-                int r = curr[0];
-                int c = curr[1];
-
-                for(int[] d : dir)
+                if(nr < 0 || nr >= rows || nc < 0 || nc >= cols)
                 {
-                    int nr = r + d[0];
-                    int nc = c + d[1];
-
-                    if (nr < 0 || nr >= rows || nc < 0 || nc >= cols)
-                    {
-                        continue;
-                    }
-
-                    if (maze[nr][nc] == '+')
-                    {
-                        continue;
-                    }
-
-                    if (nr == 0 || nr == rows - 1 || nc == 0 || nc == cols - 1)
-                    {
-                        return steps + 1;
-                    }
-                    maze[nr][nc] = '+';
-
-                    q.offer(new int[]{nr, nc});
+                    continue;
                 }
+
+                if(maze[nr][nc] == '+')
+                {
+                    continue;
+                }
+
+                if(nr == 0 || nc == 0 || nr == rows-1 || nc == cols-1)
+                {
+                    return steps + 1;
+                }
+                maze[nr][nc] = '+';
+
+                q.offer(new int[]{nr,nc});
             }
-            steps++;
         }
-        return -1;
+        steps++;
+
+    }
+
+    return -1;
+
 
     }
 }
+
+
