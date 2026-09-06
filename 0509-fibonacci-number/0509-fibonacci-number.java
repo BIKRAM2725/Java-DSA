15488c6b60d22c7722a73395fb5ef98e304d4773
BIKRAM GHOSH <ghoshbikram2725@gmail.com>
2026-09-06 11:47:22 +0530

    Time: 0 ms (100%), Space: 41.8 MB (85.5%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,31 +1,51 @@
-class Solution { 
-    public int fib(int n) { 
+// class Solution { 
+//     public int fib(int n) { 
          
-        int ans = 0; 
-        int a = 0; 
-        int b = 0; 
+//         int ans = 0; 
+//         int a = 0; 
+//         int b = 0; 
  
-        for (int i = 0; i <= n; i++) { 
+//         for (int i = 0; i <= n; i++) { 
             
-            int temp = 0; 
+//             int temp = 0; 
      
-            if (i == 0) { 
-                a = 0; 
-                ans = 0;
-            } 
-            else if (i == 1) { 
-                a = 0; 
-                b = 1; 
-                ans = 1;
-            } 
-            else { 
-                temp = ans; 
-                ans = a + b; 
-                a = b; 
-                b = ans; 
-            } 
-        } 
+//             if (i == 0) { 
+//                 a = 0; 
+//                 ans = 0;
+//             } 
+//             else if (i == 1) { 
+//                 a = 0; 
+//                 b = 1; 
+//                 ans = 1;
+//             } 
+//             else { 
+//                 temp = ans; 
+//                 ans = a + b; 
+//                 a = b; 
+//                 b = ans; 
+//             } 
+//         } 
         
-        return ans;    
-    } 
+//         return ans;    
+//     } 
+// }
+
+
+class Solution {
+    public int fib(int n) {
+
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+
+        int a = 0;
+        int b = 1;
+
+       for(int  i = 2 ; i <= n ; i++)
+       {
+        int temp = a + b;
+            a = b;
+            b = temp;
+       }
+       return b;
+    }
 }
