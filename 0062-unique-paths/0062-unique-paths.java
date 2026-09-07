b805f5bb3c8b1a0fe5fb54b43d4f4582851d7b66
BIKRAM GHOSH <ghoshbikram2725@gmail.com>
2026-09-07 11:51:53 +0530

    Time: 0 ms (100%), Space: 42.3 MB (12.89%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,22 +1,63 @@
+// // Dynamic Programming — Tabulation (Bottom-Up DP)
+
+// class Solution {
+//     public int uniquePaths(int m, int n) {
+
+//         int ans = 0;
+
+//         int[][] arr = new int[m][n];
+
+//         for(int i = 0 ; i < m; i++)
+//         {
+//             for(int j = 0 ; j < n ; j++)
+//             {
+//                 if(i == 0 || j == 0)
+//                 {
+//                     arr[i][j] = 1;
+//                 }
+//                 else
+//                 {
+//                     arr[i][j] = arr[i][j-1] + arr[i-1][j];
+//                 }
+//             }
+//         }
+//         return arr[m-1][n-1];
+        
+//     }
+// }
+
+
+// Recurtion
+
 class Solution {
     public int uniquePaths(int m, int n) {
-        
-        int[][] dp = new int[m][n];
+       
+       int[][] arr = new int[m][n];
 
-        for(int i = 0 ; i < m ; i++)
+       for(int i = 0 ; i < m ; i++)
+       {
+        for(int j = 0; j < n ; j++)
         {
-            for(int j = 0; j < n ; j++)
-            {
-                if(i == 0 || j == 0)
-                {
-                    dp[i][j] = 1;
-                }
-                else
-                {
-                    dp[i][j] = ((dp[i-1][j]) + (dp[i][j-1]));
-                }
-            }
+            arr[i][j] = -1;
+
         }
-        return dp[m-1][n-1];
+       }
+
+       return helper(0,0,m,n, arr);
+        
+    }
+
+    public static int helper(int i, int j, int m, int n, int[][] arr)
+    {
+        if(i >= m || j >= n) return 0;
+        if(i == m-1 && j == n-1) return 1;
+
+        if(arr[i][j] != -1) return arr[i][j];
+
+        int right = helper(i, j+1, m, n, arr); 
+        int down = helper(i+1, j, m, n, arr);
+
+        return arr[i][j] = right + down;
+
     }
 }
