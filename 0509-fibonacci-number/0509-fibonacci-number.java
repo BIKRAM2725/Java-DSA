355c274dd5f0d1e63b44c1e427dd9b656ceadc1c
BIKRAM GHOSH <ghoshbikram2725@gmail.com>
2026-09-07 18:48:52 +0530

    Time: 0 ms (100%), Space: 42.2 MB (16.01%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,35 +1,4 @@
-// class Solution { 
-//     public int fib(int n) { 
-         
-//         int ans = 0; 
-//         int a = 0; 
-//         int b = 0; 
- 
-//         for (int i = 0; i <= n; i++) { 
-            
-//             int temp = 0; 
-     
-//             if (i == 0) { 
-//                 a = 0; 
-//                 ans = 0;
-//             } 
-//             else if (i == 1) { 
-//                 a = 0; 
-//                 b = 1; 
-//                 ans = 1;
-//             } 
-//             else { 
-//                 temp = ans; 
-//                 ans = a + b; 
-//                 a = b; 
-//                 b = ans; 
-//             } 
-//         } 
-        
-//         return ans;    
-//     } 
-// }
-
+// Bottom-Up DP (Tabulation)
 
 // class Solution {
 //     public int fib(int n) {
@@ -51,26 +20,75 @@
 // }
 
 
+// // Recartion + Memorisation  =  Top Down DP
+
+// class Solution {
+
+//     public int fibo (int n, int[] arr)
+//     {
+//         if (n == 0) return 0;
+//         if (n == 1) return 1;
+        
+//         if(arr[n] != 0) return arr[n]; // new
+
+//         int ans = fibo(n-1, arr) + fibo(n-2, arr);
+
+//         arr[n] = ans; // new
+
+//         return ans;
+
+//     }
+//     public int fib(int n) {
+       
+//        int[] arr = new int[n + 1];
+//        return fibo(n, arr);
+//     }
+// }
+
+
+
+// Tabulation [ You can do using array ] 
+
+// class Solution {
+
+//     public int fib(int n) {
+
+//         if (n <= 1) return n;
+
+//         int[] dp = new int[n + 1];
+
+//         dp[0] = 0;
+//         dp[1] = 1;
+
+//         for (int i = 2; i <= n; i++) {
+//             dp[i] = dp[i - 1] + dp[i - 2];
+//         }
+
+//         return dp[n];
+//     }
+// }
+
+
+
+// // Space Optimisation ( Tabulation )
 
 class Solution {
 
-    public int fibo (int n, int[] arr)
-    {
-        if (n == 0) return 0;
-        if (n == 1) return 1;
-        
-        if(arr[n] != 0) return arr[n]; // new
-
-        int ans = fibo(n-1, arr) + fibo(n-2, arr);
-
-        arr[n] = ans; // new
-
-        return ans;
-
-    }
     public int fib(int n) {
-       
-       int[] arr = new int[n + 1];
-       return fibo(n, arr);
+
+        if (n <= 1) return n;
+
+        int[] dp = new int[3];
+
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            dp[2] = dp[1] + dp[0];
+            dp[0] = dp[1];
+            dp[1] = dp[2];
+        }
+
+        return dp[2];
     }
 }
