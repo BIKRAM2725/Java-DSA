05ef229d37f6ad735e585513470e4879b630b70b
BIKRAM GHOSH <ghoshbikram2725@gmail.com>
2026-09-19 21:27:16 +0530

    Time: 1 ms (47.09%), Space: 46.6 MB (37.56%) - LeetHub

---
+++ 0589-n-ary-tree-preorder-traversal/0589-n-ary-tree-preorder-traversal.java
@@ -0,0 +1,42 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+    public List<Integer> preorder(Node root) {
+
+    List<Integer> arr = new ArrayList<>();
+
+    preorder(root, arr);
+
+    return arr;    
+
+    }
+
+    public void preorder(Node root, List<Integer> arr)
+    {
+        if(root == null) return;
+
+        arr.add(root.val);
+        
+        for(Node c : root.children)
+        {
+            preorder(c, arr);
+        }
+    }
+}
