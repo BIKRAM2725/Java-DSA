78032195d2022efa3c7d5db27776a385a84ba61d
BIKRAM GHOSH <ghoshbikram2725@gmail.com>
2026-09-23 18:33:54 +0530

    Time: 2 ms (92.6%), Space: 72.1 MB (43.87%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,92 +1,25 @@
-// class Solution {
-//     public int[] productExceptSelf(int[] nums) {
-
-//         int n = nums.length;
-
-//         int[] ans = new int[n];
-
-//         ans[0] = 1;
-
-//         for(int i = 1 ; i < n ; i++)
-//         {
-//             ans[i] = ans[i-1] * nums[i-1];
-            
-//         }
-//         int right = 1;
-
-//         for(int i = n-1 ; i >= 0 ; i--)
-//         {
-//             ans[i] = ans[i] * right;
-//             right = right * nums[i];
-//         }
-//         return ans;
-//     }
-// }
-
-
-
-
-// Divide not accected but
-
 class Solution {
     public int[] productExceptSelf(int[] nums) {
 
         int n = nums.length;
 
-        int zero = 0;
+        int[] arr = new int[n];
 
-        int product = 1;
+        arr[0] = 1;
 
-        int[] ans = new int[n];
-
-        for(int i : nums)
+        for(int i = 1 ; i < n ; i++)
         {
-            if(i == 0)
-            {
-                zero++;
-            }
-            else
-            {
-                product *= i;
-            }
+            arr[i] = arr[i-1] * nums[i-1];
         }
 
-        if(zero == 1)
-        {
-            for(int i = 0 ; i < n ; i++)
-            {
-                if(nums[i] == 0)
-                {
-                    ans[i] = product;
-                }
-                else
-                {
-                    ans[i] = 0;
-                }
-            }
-        }
-        else if (zero >= 2)
-        {
-             for(int i = 0 ; i < n ; i++)
-            {
-                
-                ans[i] = 0;
-            
-            }
+        int right = 1;
 
-        }
-        else
+        for(int i = nums.length-1 ; i >= 0 ; i--)
         {
-             for(int i = 0 ; i < n ; i++)
-            {
-                
-                    ans[i] = product / nums[i];
-                
-            }
-
+            arr[i] = right * arr[i];
+            right = right * nums[i];
         }
-        return ans;
+
+        return arr;
     }
 }
-
-
