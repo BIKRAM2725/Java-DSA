97d87632110a519fa7c08e13d7ef3a9f2c0e29f3
BIKRAM GHOSH <ghoshbikram2725@gmail.com>
2026-09-25 23:45:36 +0530

    Time: 1 ms (95.7%), Space: 42.5 MB (90.32%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.java
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -1,29 +1,22 @@
 class Solution {
     public boolean isSubsequence(String s, String t) {
 
-        if(s.length()==0) return true;
-
-        char[] arr1 = s.toCharArray();
-        char[] arr2 = t.toCharArray();
-
-        int n = arr1.length;
+        if (s.length() == 0) return true;
+        if (s.length() > t.length()) return false;
 
         int p = 0;
-        
-        for(int i = 0 ; i < arr1.length ;i++)
-        {
-            for(int j = p ; j < arr2.length ; j++ )
-            {
-                if(arr1[i] == arr2[j])
-                {
-                    n--;
-                    p = j + 1;
+        int check = 0;
 
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = p; j < t.length(); j++) {
+                if (s.charAt(i) == t.charAt(j)) {
+                    p = j + 1;
+                    check++;
                     break;
                 }
             }
-            if(n <= 0) return true;
         }
-        return false;
+
+        return check == s.length();
     }
 }
